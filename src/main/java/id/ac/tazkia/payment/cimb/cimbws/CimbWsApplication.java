a36239e80ab960c167eab9c8878c96052631bfb3
Endy Muhardin <endy.muhardin@gmail.com>
2018-06-28 11:16:59 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/payment/cimb/cimbws/CimbWsApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.payment.cimb.cimbws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CimbWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CimbWsApplication.class, args);
+	}
+}
---
+++ src/main/resources/db/migration/V1.0.0.20180628__Skema_Awal.sql
@@ -0,0 +1,30 @@
+CREATE TABLE virtual_account (
+  id                 VARCHAR(36),
+  account_number     VARCHAR(255)   NOT NULL,
+  invoice_number     VARCHAR(255)   NOT NULL,
+  name               VARCHAR(255)   NOT NULL,
+  account_type       VARCHAR(255)   NOT NULL,
+  amount             NUMERIC(19, 2) NOT NULL,
+  cumulative_payment NUMERIC(19, 2) NOT NULL DEFAULT 0,
+  description        VARCHAR(255),
+  email              VARCHAR(255),
+  phone              VARCHAR(255),
+  create_time        TIMESTAMP      NOT NULL,
+  expire_date        DATE           NOT NULL,
+  account_status     VARCHAR(255)   NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (invoice_number)
+);
+
+CREATE TABLE payment (
+  id                    VARCHAR(36),
+  id_virtual_account    VARCHAR(36)    NOT NULL,
+  amount                NUMERIC(19, 2) NOT NULL,
+  transaction_time      TIMESTAMP      NOT NULL,
+  transaction_reference VARCHAR(36)    NOT NULL,
+  client_reference      VARCHAR(36)    NOT NULL,
+  payment_status        VARCHAR(255)   NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_virtual_account) REFERENCES virtual_account (id),
+  UNIQUE (id_virtual_account,client_reference)
+);
