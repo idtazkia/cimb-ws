a3e26cf415045004c0658568fc6c0eb3ff6c3984
Endy Muhardin <endy.muhardin@gmail.com>
2018-08-20 15:52:42 +0700

    setup continuous deployment

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+APPNAME=cimb-ws
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Cara pakai : deploy.sh <namafile.jar>'
+    exit 1
+fi
+
+service $APPNAME stop
+rm /var/lib/$APPNAME/$APPNAME.jar
+ln -s /var/lib/$APPNAME/$1 /var/lib/$APPNAME/$APPNAME.jar
